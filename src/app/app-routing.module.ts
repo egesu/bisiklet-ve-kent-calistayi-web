a51bafd58c23f2df95b7c19dbcec72e6d357eed2
Ege <alikemal@riseup.net>
2017-10-08 17:52:35 +0300

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { ReasonsComponent } from './reasons/reasons.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { OrganizersComponent } from './organizers/organizers.component';
+import { DocumentsComponent } from './documents/documents.component';
+
 const routes: Routes = [
   {
     path: '',
-    children: []
-  }
+    component: HomeComponent,
+  },
+  {
+    path: 'neden',
+    component: ReasonsComponent,
+  },
+  {
+    path: 'program',
+    component: ScheduleComponent,
+  },
+  {
+    path: 'paydaslar',
+    component: OrganizersComponent,
+  },
+  {
+    path: 'belgeler',
+    component: DocumentsComponent,
+  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SpinnerComponentModule } from 'ng2-component-spinner';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ReasonsComponent } from './reasons/reasons.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { OrganizersComponent } from './organizers/organizers.component';
+import { DocumentsComponent } from './documents/documents.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ReasonsComponent,
+    ScheduleComponent,
+    OrganizersComponent,
+    DocumentsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+
+    NgbModule.forRoot(),
+    SpinnerComponentModule,
+
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
