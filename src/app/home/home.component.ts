88dfe1ed5c5e6b2ceb60a7c9c2545ef254129557
Ege <alikemal@riseup.net>
2017-10-21 01:12:42 +0300

    Added ability to add live stream

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 import { ConfigurePiwikTracker, UsePiwikTracker } from 'angular2piwik';
 
@@ -11,16 +12,22 @@ import { environment } from '../../environments/environment';
   providers: [UsePiwikTracker, ConfigurePiwikTracker],
 })
 export class HomeComponent implements OnInit {
+  public liveStreamUrl: SafeResourceUrl;
 
   constructor(
     private piwik: UsePiwikTracker,
-    private piwikConfigure: ConfigurePiwikTracker
+    private piwikConfigure: ConfigurePiwikTracker,
+    private sanitizer: DomSanitizer
   ) { }
 
   ngOnInit() {
     if (!environment.production) {
       this.piwikConfigure.setDocumentTitle();
       this.piwik.trackPageView();
+    }
+
+    if (environment.liveStream) {
+      this.liveStreamUrl = this.sanitizer.bypassSecurityTrustResourceUrl(environment.liveStream);
     }
   }
 
